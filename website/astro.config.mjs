7c1519a6c4e0430c68143227fb53f9b5bc0437b1
Multipixelone <finn@cnwr.net>
2025-10-11 00:07:41 -0400

    switch to using astro's font api

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 // export default defineConfig({});
 export default defineConfig({
+      experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Public Sans",
+            cssVariable: "--font-public-sans"
+        },
+      {
+        
+            provider: fontProviders.google(),
+            name: "Caprasimo",
+            cssVariable: "--font-caprasimo"
+      }
+    ]
+    },
   vite: {
     // Vite-specific configurations go here
     plugins: [
