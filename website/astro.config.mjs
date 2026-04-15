c448dd1bd938ae2ba09cb176987de13809326ac4
Multipixelone <finn@cnwr.net>
2026-04-15 13:01:54 -0400

    chore(deps): bump astro to 6.1.2

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,38 +2,33 @@
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-// export default defineConfig({});
 export default defineConfig({
-      experimental: {
-        fonts: [{
-      provider: "local",
-            name: "Public Sans",
-            cssVariable: "--font-public-sans",
-          variants: [
-        {
-        src: [
-              "./src/assets/fonts/open-sans-v44-latin-regular.woff2"
-            ]  
-        },
-        
-      ]
-        },
-      {
-        
-            provider: "local",
-            name: "Caprasimo",
-            cssVariable: "--font-caprasimo",
-          variants: [
-        {
-        src: [
-              "./src/assets/fonts/caprasimo-v6-latin-regular.woff2"
-            ]  
-        },
-        
-      ]
-      }
-    ]
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Public Sans",
+      cssVariable: "--font-public-sans",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/open-sans-v44-latin-regular.woff2"],
+          },
+        ],
+      },
     },
+    {
+      provider: fontProviders.local(),
+      name: "Caprasimo",
+      cssVariable: "--font-caprasimo",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/caprasimo-v6-latin-regular.woff2"],
+          },
+        ],
+      },
+    },
+  ],
   vite: {
     // Vite-specific configurations go here
     plugins: [
